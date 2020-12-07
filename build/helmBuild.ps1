2928ceba59db4e4e321d16b691e7355b5b204530
deaborch <44680211+deaborch@users.noreply.github.com>
2020-12-07 14:34:41 -0800

    fixed docs

--- build/helmBuild.ps1
+++ build/helmBuild.ps1
@@ -4,4 +4,4 @@ Set-Location $workingDir
 
 helm package guard-intergration\
 
-helm repo index guard-intergration --url https://abdelacharts.blob.core.windows.net/guard-intergration
+helm repo index guard-intergration --url https://deaborch.github.io/aks-engine-guard-integration
